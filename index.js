a31e235949c9cd4682902e7b507fe09815091442
Diocleniax789 <julioramdom123@gmail.com>
2025-03-10 23:43:58 -0300

    se han agregado la  funcionalidad para mostrar las imagenes

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const imagenPortada = document.getElementById('portada-libro');
+const arregloImagenesPorNumero = [];
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function cambiarImagen() {
 	let nroPortada = 1;
+	let i = 0;
 	while (true) {
+
+		arregloImagenesPorNumero.push(nroPortada);
+
 		await delay(1500);
 		imagenPortada.classList.add('imagen-inicio-oscurecer');
 		
 		await delay(1000); 
-		nroPortada = nroPortada === 1 ? 2 : 1;
-		imagenPortada.src = `images/mangas-inicio/${nroPortada}.jpg`;
+		
+		imagenPortada.src = `images/mangas-inicio/${arregloImagenesPorNumero[i]}.jpg`;
 		
 		await delay(50); 
 		imagenPortada.classList.remove('imagen-inicio-oscurecer');
-	}
-}
+		
+		nroPortada++;
+		i++;			
+	
+		// corregir el tema de las de las transiciones
+	}}
 
 cambiarImagen();
