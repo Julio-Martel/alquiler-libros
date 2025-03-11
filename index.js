3f3bf86212881bda1de7b4211c5aea3ad2ea6dbd
Diocleniax789 <julioramdom123@gmail.com>
2025-03-10 22:37:15 -0300

    se ha podido implementar el cambio de portada de libro automatico

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-let portadaCambiar = document.getElementById('portada-libro');
+const contenedorImagen = document.querySelector('.contenedor-imagen');
+
+console.log(contenedorImagen);
+
+const delay = (ms) => {
+	return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+async function cambiarImagen() {
+	let nroPortada = 1;
+	while(nroPortada === 1 || nroPortada === 2) {
+		let portada = `<img src="images/mangas-inicio/${nroPortada}.jpg" class="imagen-inicio" id="portada-libro">`;
+		await delay(1500);	
+		nroPortada++;
+		contenedorImagen.innerHTML = portada;
+		if (nroPortada > 2) {nroPortada = 1}
+	}
+}
+
+
+cambiarImagen();
