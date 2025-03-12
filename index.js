b54b69931ef85a967cfc903351019df588252d1f
Diocleniax789 <julioramdom123@gmail.com>
2025-03-11 21:07:56 -0300

    se ha mejorado la funcion para la muestra de las portadas de los libros

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const imagenPortada = document.getElementById('portada-libro');
-const arregloImagenesPorNumero = [];
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function cambiarImagen() {
 	let nroPortada = 1;
-	let i = 0;
-	while (true) {
 
-		arregloImagenesPorNumero.push(nroPortada);
+	while (true) {
 
 		await delay(1500);
 		imagenPortada.classList.add('imagen-inicio-oscurecer');
 		
-		await delay(1000); 
+		await delay(1000); 		
 		
-		imagenPortada.src = `images/mangas-inicio/${arregloImagenesPorNumero[i]}.jpg`;
+		nroPortada++;
+
+		if (nroPortada > 3) {nroPortada = 1}
+
+		imagenPortada.src = `images/mangas-inicio/${nroPortada}.jpg`;
 		
 		await delay(50); 
 		imagenPortada.classList.remove('imagen-inicio-oscurecer');
 		
-		nroPortada++;
-		i++;			
+				
 	
 		// corregir el tema de las de las transiciones
 	}}
